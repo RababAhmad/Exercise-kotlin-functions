cc1c92e430999eed3f2e894dfdc3c692cf909e59
Rabab <raaa555@outlook.com>
2025-02-12 18:41:41 +0300

    Added several functions in this exercise

--- functions/src/Main.kt
+++ functions/src/Main.kt
@@ -7,6 +7,16 @@ fun main(){
     val afternoonTemperature = 40
     val averageTemp = currentTemperature(morningTemperature, afternoonTemperature)
     println("The average temperature for the day is: $averageTemp")
+    startCamelRide(10, 5)
+    startCamelRide(10)
+   // exploreDune(height = 20.0, climRate = 3.0)
+    val time = exploreDune(climRate = 3.0, height = 20.0)
+    println("The time to climb the dune is: $time")
+
+  //  survivalChance(supplies = 8)
+    val supplies = survivalChance(supplies = 8)
+    println(" supplies chance: $supplies")
+
 }
 
 fun greetTraveler(){
@@ -24,4 +34,21 @@ fun currentTemperature(morningTemperature: Int, afternoonTemperature: Int): Int 
 
     return (morningTemperature + afternoonTemperature) / 2
 
+}
+
+fun startCamelRide(time: Int, speed: Int = 5){
+    val distance = (speed * time)
+    println("camel Ride: Distance = $distance km, speed = $speed km/h, Time = $time hours ")
+
+
+
+}
+
+fun exploreDune(height: Double = 20.0, climRate: Double = 3.0) : Double {
+    return height/climRate // to calculate the time to climb the dune
+
+}
+
+fun survivalChance(supplies: Int): Int {
+    return supplies * 10
 }
